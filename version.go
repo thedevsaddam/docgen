32ed42b5db8d0371248ac61b836a0d2ab27f9652
Saddam H <thedevsaddam@gmail.com>
2019-02-12 11:29:56 +0600

    Update version command

--- version.go
+++ version.go
@@ -11,7 +11,18 @@ var versionCmd = &cobra.Command{
 	Short: "Provide version information",
 	Long:  `Provide version information`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Docgen version: v2.0")
+		const logo = `
+  _____                _____
+ |  __ \              / ____|
+ | |  | | ___   ___  | |  __  ___ _ __
+ | |  | |/ _ \ / __| | | |_ |/ _ \ '_ \
+ | |__| | (_) | (__  | |__| |  __/ | | |
+ |_____/ \___/ \___|  \_____|\___|_| |_|
+                                                                                
+`
+		fmt.Println(logo)
+		fmt.Println("Docgen version: v2.1")
 		fmt.Println("Support postman collection version > 2.1")
+		fmt.Println("For more info visit: https://github.com/thedevsaddam/docgen")
 	},
 }
