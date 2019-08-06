4901ac4570cb0c1a1dbbf88f1718325da743026e
Enrico204 <enrico204@gmail.com>
2019-08-06 17:39:36 +0200

    Remove go-bindata as it doesn't exists anymore, load directly from file

--- funcmap.go
+++ funcmap.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"regexp"
@@ -13,7 +14,7 @@ import (
 )
 
 func getData(a string) string {
-	bs, err := Asset(a)
+	bs, err := ioutil.ReadFile(a)
 	if err != nil {
 		log.Fatal(err)
 	}
